b861267c79d26633cd15593c8440a945be962b47
pax5837 <46838238+pax5837@users.noreply.github.com>
2024-12-08 19:17:49 +0100

    wip 2024 004

---
+++ AdventOfCode2024-005/Part1.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections.Immutable;
+using System.ComponentModel.Design;
+
+namespace AdventOfCode2024_005;
+
+internal static class Part1
+{
+    public static void Solve(
+        IImmutableList<string> pageOrderingRulesLines,
+        IImmutableList<string> pagesToProduceLines)
+    {
+        var precedingPagesByPage = GetPrecedingPagesByPage(pageOrderingRulesLines);
+        var pagesToProduce = pagesToProduceLines
+            .Select(line => line
+                .Split(",", StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToImmutableList())
+            .ToImmutableList();
+
+        var pagesToProduceWithCorrectOrdering = pagesToProduce
+            .Where(ptp => IsOrderingRespected(ptp, precedingPagesByPage))
+            .ToImmutableList();
+
+        // foreach (var x in pagesToProduceWithCorrectOrdering)
+        // {
+        //     Console.WriteLine(string.Join(", ", x));
+        // }
+        
+        pagesToProduceWithCorrectOrdering
+            .Select(pstp => pstp.)
+
+
+
+    }
+
+    private static bool IsOrderingRespected(ImmutableList<int> pagesToProduce, ILookup<int, int> precedingPagesByPage)
+    {
+        return pagesToProduce
+            .All(ptp => IsOrderingRespected(ptp, pagesToProduce,  precedingPagesByPage));
+    }
+
+    private static bool IsOrderingRespected(int currentPage, ImmutableList<int> pagesToProduce, ILookup<int, int> precedingPagesByPage)
+    {
+        var indexOfCurrent = pagesToProduce.IndexOf(currentPage);
+        var precedingPages = precedingPagesByPage[currentPage].ToImmutableHashSet();
+        return pagesToProduce
+            .Select((ptp, idx) => (ptp, idx))
+            .All(pair => !precedingPages.Contains(pair.ptp) || pair.idx < indexOfCurrent);
+        
+    }
+
+
+    private static ILookup<int, int> GetPrecedingPagesByPage(IImmutableList<string> pageOrderingRulesLines)
+    {
+        var precedingPagesByPage = pageOrderingRulesLines
+            .Select(line => line.Split("|", StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries))
+            .Select(x => (mustComeBefore: int.Parse(x[0]), refPage: int.Parse(x[1])))
+            .ToLookup(x => x.refPage, x => x.mustComeBefore);
+        
+        // foreach (var bla in precedingPagesByPage)
+        // {
+        //     Console.WriteLine($"{bla.Key} preceded by {string.Join(", ", bla.Select(y => y))}");
+        // }
+        
+        return precedingPagesByPage;
+    }
+}
---
+++ AdventOfCode2024-005/Program.cs
@@ -0,0 +1,11 @@
+﻿// https://adventofcode.com/2024/day/5
+
+
+using System.Collections.Immutable;
+using AdventOfCode2024_005;
+
+var folder = "ExampleData";
+var pageOrderingRulesLines = File.ReadAllLines($"./{folder}/PageOrderingRules.txt").ToImmutableList();
+var pagesToProduceLines = File.ReadAllLines($"./{folder}/PagesToProduce.txt").ToImmutableList();
+
+Part1.Solve(pageOrderingRulesLines, pagesToProduceLines);
