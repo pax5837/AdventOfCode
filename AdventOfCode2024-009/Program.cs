7db704ba0b9d81569312f71ea52a51b9e1e69bbb
patrick5837 <patrick5837@gmail.com>
2024-12-29 23:31:24 +0100

    2024 day 9 improve output

--- AdventOfCode2024-009/Program.cs
+++ AdventOfCode2024-009/Program.cs
@@ -4,20 +4,24 @@ using System.Collections.Immutable;
 using System.Diagnostics;
 using AdventOfCode2024_009;
 
+const bool isExample = false;
+
 const string exampleFolder = "ExampleData";
-var folder = exampleFolder;
-folder = "InputData";
+const string inputFolder = "InputData";
+var folder = isExample ? exampleFolder : inputFolder;
 var lines = File.ReadAllLines($"./{folder}/input.txt").ToImmutableList();
 
+var expectedPart1 = isExample ? "1928" : "6398252054886";
+var expectedPart2 = isExample ? "2858" : "6415666220005";
 
-var isExample = folder == exampleFolder;
-Execute("Part 1, should return 6398252054886", () => Part1.Process(lines[0], false && isExample));
-Execute("Part 1 V2, should return 6398252054886", () => Part1V2.Process(lines[0], false && isExample));
-// Execute("Part 2, should return 6415666220005", () => Part2.Process(lines[0], true && isExample));
-Execute("Part 2 V2, should return 6415666220005", () => Part2V2.Process(lines[0], true && isExample));
-Execute("Part 2 V3, should return 6415666220005", () => Part2V3.Process(lines[0], true && isExample));
 
-void Execute(string name, Func<string> func)
+Execute("Part 1", expectedPart1, () => Part1.Process(lines[0], false && isExample));
+Execute("Part 1 V2", expectedPart1, () => Part1V2.Process(lines[0], false && isExample));
+// Execute("Part 2", expectedPart2, () => Part2.Process(lines[0], true && isExample));
+Execute("Part 2 V2", expectedPart2, () => Part2V2.Process(lines[0], true && isExample));
+Execute("Part 2 V3", expectedPart2, () => Part2V3.Process(lines[0], true && isExample));
+
+void Execute(string name, string expected, Func<string> func)
 {
 	const int totalWidth = 50;
 	Console.WriteLine(string.Empty.PadLeft(totalWidth, '#'));
@@ -25,6 +29,7 @@ void Execute(string name, Func<string> func)
 	var sw = Stopwatch.StartNew();
 	var result = func();
 	sw.Stop();
-	Console.WriteLine($" Result: <{result}> in {sw.ElapsedMilliseconds} ms");
+	var okText = result == expected ? "OK" : "NOT OK";
+	Console.WriteLine($"{okText}, expected: <{expected}>, result: <{result}>, in {sw.ElapsedMilliseconds} ms");
 	Console.WriteLine("\n".PadLeft(totalWidth, '#'));
 }
