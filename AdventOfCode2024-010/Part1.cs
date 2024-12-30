a2594a45452cddc5db9bc1002b3a5b068d10489e
patrick5837 <patrick5837@gmail.com>
2024-12-30 07:26:16 +0100

    2024 Day 10, initial project setup

---
+++ AdventOfCode2024-010/Part1.cs
@@ -0,0 +1,11 @@
+using System.Collections.Immutable;
+
+namespace AdventOfCode2024_010;
+
+internal static class Part1
+{
+	public static string Process(IImmutableList<string> lines, bool withConsoleOutput = false)
+	{
+		return string.Empty;
+	}
+}
---
+++ AdventOfCode2024-010/Program.cs
@@ -0,0 +1,30 @@
+﻿// https://adventofcode.com/2024/day/10
+
+using System.Collections.Immutable;
+using System.Diagnostics;
+using AdventOfCode2024_010;
+
+const bool isExample = true;
+
+const string exampleFolder = "ExampleData";
+const string inputFolder = "InputData";
+var folder = isExample ? exampleFolder : inputFolder;
+var lines = File.ReadAllLines($"./{folder}/input.txt").ToImmutableList();
+
+var expectedPart1 = isExample ? "36" : "??";
+var expectedPart2 = isExample ? "??" : "??";
+
+Execute("Part 1", expectedPart1, () => Part1.Process(lines, false && isExample));
+
+void Execute(string name, string expected, Func<string> func)
+{
+	const int totalWidth = 50;
+	Console.WriteLine(string.Empty.PadLeft(totalWidth, '#'));
+	Console.WriteLine($"[{name}]");
+	var sw = Stopwatch.StartNew();
+	var result = func();
+	sw.Stop();
+	var okText = result == expected ? "OK" : "NOT OK";
+	Console.WriteLine($"{okText}, expected: <{expected}>, result: <{result}>, in {sw.ElapsedMilliseconds} ms");
+	Console.WriteLine("\n".PadLeft(totalWidth, '#'));
+}
